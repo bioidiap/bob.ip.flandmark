ae831088123d62a0ca741792afd9a05f42b17025
Andre Anjos <andre.dos.anjos@gmail.com>
2013-02-05 12:26:23 +0100

    Simplified setup infrastructure

--- setup.py
+++ setup.py
@@ -6,90 +6,8 @@
 """Bindings for flandmark
 """
 
-import sys
-import subprocess
 from setuptools import setup, find_packages
-from distutils.extension import Extension
-
-def pkgconfig(package):
-
-  def uniq(seq, idfun=None):
-    # order preserving
-    if idfun is None:
-      def idfun(x): return x
-    seen = {}
-    result = []
-    for item in seq:
-      marker = idfun(item)
-      # in old Python versions:
-      # if seen.has_key(marker)
-      # but in new ones:
-      if marker in seen: continue
-      seen[marker] = 1
-      result.append(item)
-    return result
-
-  flag_map = {
-      '-I': 'include_dirs',
-      '-L': 'library_dirs',
-      '-l': 'libraries',
-      }
-
-  cmd = [
-      'pkg-config',
-      '--libs',
-      '--cflags',
-      package,
-      ]
-
-  proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-      stderr=subprocess.STDOUT)
-
-  output = proc.communicate()[0]
-
-  if proc.returncode != 0:
-    raise RuntimeError, "PkgConfig did not find package %s. Output:\n%s" % \
-        (package, output.strip())
-
-  kw = {}
-
-  for token in output.split():
-    if flag_map.has_key(token[:2]):
-      kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-
-    elif token[0] == '-': # throw others to extra_link_args
-      kw.setdefault('extra_compile_args', []).append(token)
-
-    else: # these are maybe libraries
-      if os.path.exists(token):
-        dirname = os.path.dirname(token)
-        if dirname not in kw.get('library_dirs', []):
-          kw.setdefault('library_dirs', []).append(dirname)
-
-        bname = os.path.splitext(os.path.basename(token))[0][3:]
-        if bname not in kw.get('libraries', []):
-          kw.setdefault('libraries', []).append(bname)
-
-  for k, v in kw.iteritems(): # remove duplicated
-    kw[k] = uniq(v)
-
-  return kw
-
-def setup_bob_extension(ext_name, sources):
-  """Sets up a given C++ extension that depends on Bob"""
-
-  bob = pkgconfig('bob-python')
-  ocv = pkgconfig('opencv')
-
-  return Extension(
-      ext_name,
-      sources=sources,
-      language="c++",
-      include_dirs=bob.get('include_dirs',[]) + ocv.get('include_dirs',[]),
-      library_dirs=bob.get('library_dirs',[]) + ocv.get('library_dirs', []),
-      runtime_library_dirs=bob.get('library_dirs',[]) + ocv.get('library_dirs', []),
-      libraries=bob.get('libraries',[]) + ocv.get('libraries',[]),
-      )
+from xbob.extension import Extension
 
 setup(
 
@@ -110,6 +28,10 @@ setup(
       "xbob",
       ],
 
+    setup_requires=[
+      'xbob.extension',
+      ],
+
     install_requires=[
       'setuptools',
       'bob',
@@ -122,12 +44,16 @@ setup(
       },
 
     ext_modules=[
-      setup_bob_extension("xbob.flandmark._flandmark",
+      Extension("xbob.flandmark._flandmark",
         [
           "xbob/flandmark/ext/flandmark_detector.cpp",
           "xbob/flandmark/ext/liblbp.cpp",
           "xbob/flandmark/ext/ext.cpp",
-        ])
+        ],
+        bob_modules = [
+          'opencv',
+          ]
+        )
       ],
 
     classifiers = [
